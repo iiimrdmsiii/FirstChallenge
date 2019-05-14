41ebfeb1b2bc4a8015297b64bfd3ba9f55d97a24
dallin smuin <dallinsmuin@gmail.com>
2019-05-14 08:10:55 -0600

    add // to explane things.

--- FirstChallengeMay13,2019.playground/Contents.swift
+++ FirstChallengeMay13,2019.playground/Contents.swift
@@ -3,12 +3,13 @@
 /* Create a playground in Xcode to demonstrate basic knowledge in programming basics. Loop through numbers 0-100. For each number that is divisible by 3 print to console "Mountainland". For each number that is divisible by 5 print to console "Technical". For each number that is divisible by 3 and 5 print to console "Mountainland Technical College". For each number that isn't divisible by 3 or 5 print out the number. After the loop print to console all of the numbers that are divisible by 3 and 5.
  */
 
+// List of properties
 var arrayOfThreeAndFive: [Int] = []
-
 var mountianLandThree = "Mountainland"
 var technicalFive = "Technical"
 var mountainlandTechnicalCollege = "Mountainland Technical College"
 
+// runs the loop and displays the calulations of the MTech names.
 for number in 0...100 {
     if number % 3 == 0 && number > 0 {
         if number % 5 == 0 {
@@ -23,5 +24,7 @@ for number in 0...100 {
         print(number)
     }
 }
+
+
 
 
